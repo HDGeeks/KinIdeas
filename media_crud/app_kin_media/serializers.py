8f84ea75222dc4fe8e858636f2387d1efa5ddc7c
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-06-14 11:36:53 +0300

    done with serializations

---
+++ media_crud/app_kin_media/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from .models import Artist, Album, Track
+
+class TrackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Track
+        fields = ("album" , "track_name", "track_file", "cover_image", "duration", "created_at", "updated_at", "track_age", "track_sex", "tracks_martial_status")
+
+class AlbumSerializer(serializers.ModelSerializer):
+    tracks = TrackSerializer(many=True)
+
+    class Meta:
+        model = Album
+        fields = ("artist" , "album_name", "album_cover")
+
+    def create(self, validated_data):
+        tracks_data = validated_data.pop('tracks')
+        album = Album.objects.create(**validated_data)
+        for track_data in tracks_data:
+            Track.objects.create(album=album, **track_data)
+        return album
+
+class ArtistSerializer(serializers.ModelSerializer):
+    albums = AlbumSerializer(many=True)
+
+    class Meta:
+        model = Artist
+        fields = ("artist_id" , "arists_name", "profile_image")
+
+    def create(self, validated_data):
+        albums_data = validated_data.pop('albums')
+        artist = Artist.objects.create(**validated_data)
+        for album_data in albums_data:
+            Album.objects.create(artist=artist, **album_data)
+        return artist
